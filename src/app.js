743776dc74b7a0555b01290404b8057f3089ae93
y.kostiuk <yaroslav.kostiuk@socialtechnologies.io>
2021-06-16 21:33:16 +0300

    todo. v2

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 import './main.sass';
 
 // variables
-let addButton = document.getElementById("addBtn");
+let addButtonAction = document.getElementById("addBtn");
 let close = document.getElementsByClassName("todo__item-close");
 let list = document.querySelector('ul');
-let todoItem = [];
+let toDoItem = [];
+let getLocalStorage = JSON.parse(localStorage.getItem('toDoItem'));
+let html = function (item_text) {
+    return `
+        <label class="todo__item-label">
+            <input class="todo__item-checkbox" type="checkbox">
+            ${item_text}
+        </label>
+        <div class="todo__item-icons">
+            <img class="todo__item-icon todo__item-close" src="https://raw.githubusercontent.com/hoffmannmark/toDoList/639d36b4c8566d86eaaa657cb64608f62e6060ad/img/trash.svg" alt="">
+        </div>`
+};
 
 // Add checked status
 list.addEventListener('click', function (ev) {
@@ -21,44 +32,79 @@ input.addEventListener("keyup", function (event) {
         document.getElementById("addBtn").click();
     }
 });
-// Create a new list item when click button
-addButton.addEventListener('click', function (event) {
-    let li = document.createElement("li");
+
+// Create a new list item on click button
+addButtonAction.addEventListener('click', function (event) {
+    let liCountId = document.querySelectorAll('#itemsList > li').length;
     let addClassWarning = document.getElementById("inputText");
     let inputValue = document.getElementById("inputText").value;
+    let li = document.createElement("li");
+    li.setAttribute('data-id', liCountId);
 
-    todoItem.push(inputValue);
+    toDoItem.push(inputValue);
 
-    // li.appendChild(addItem);
     li.className = "todo__item";
-    li.innerHTML = `
-        <label class="todo__item-label">
-            <input class="todo__item-checkbox" type="checkbox">
-            ${inputValue}
-        </label>
-        <div class="todo__item-icons">
-            <img class="todo__item-icon todo__item-close" src="https://raw.githubusercontent.com/hoffmannmark/toDoList/639d36b4c8566d86eaaa657cb64608f62e6060ad/img/trash.svg" alt="">
-        </div>`;
+    li.innerHTML = html(inputValue);
 
+    // add item & warning
     if (inputValue === '') {
         addClassWarning.classList.add("todo__header-warning");
         setTimeout(() => addClassWarning.classList.remove("todo__header-warning"), 2500);
-        localStorage.setItem('toDoItem', JSON.stringify(todoItem));
     } else {
+        // return object
         document.getElementById("itemsList").appendChild(li);
-        localStorage.setItem('toDoItem', JSON.stringify(todoItem));
+        // add ket to array
+        localStorage.setItem('toDoItem', JSON.stringify(toDoItem));
     }
 
+    // clean input
     document.getElementById("inputText").value = "";
 
-    for (let i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            let div = this.parentElement.parentElement;
-            delete todoItem[this];
-        }
-    }
+    // close action
+    // for (let i = 0; i < close.length; i++) {
+    //     close[i].onclick = function () {
+    //
+    //         let element = this.parentElement.parentElement;
+    //
+    //         element.remove();
+    //
+    //         console.log(localStorage.removeItem('toDoItem'));
+    //     }
+    // }
 
 });
 
+window.addEventListener('DOMContentLoaded', function (event) {
 
 
+    initTodo()
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+
+            let element = this.parentElement.parentElement;
+            let countIndex = element.getAttribute('data-id');
+            element.remove();
+
+            var getLocalStorage = JSON.parse(localStorage.getItem("toDoItem"));
+            getLocalStorage.splice(countIndex, 1);
+            localStorage.removeItem('toDoItem');
+            localStorage.setItem('toDoItem', JSON.stringify(getLocalStorage));
+            initTodo()
+        }
+    }
+
+})
+
+function initTodo() {
+    for (let i = 0; i < getLocalStorage.length; i++) {
+        if (getLocalStorage.length !== null) {
+            let liCountId = document.querySelectorAll('#itemsList > li').length;
+            let itm = document.createElement("li");
+            itm.setAttribute('data-id', liCountId);
+            list.appendChild(itm);
+            itm.className = 'todo__item';
+            itm.innerHTML = html(getLocalStorage[i]);
+
+        }
+    }
+}
